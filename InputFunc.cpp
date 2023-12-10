ff0e6817625ab8f7885b5b971c8d85c34f9f8561
Jon <118042236+j9227601188@users.noreply.github.com>
2023-12-10 20:19:47 +0300

    Create InputFunc.cpp

---
+++ InputFunc.cpp
@@ -0,0 +1,71 @@
+#include "InputFunc.h"
+
+void inputFunc()
+{
+    TreeDictionary test;
+    test.test_content();
+    
+    //to select words
+    std::vector<std::string> words;
+    test._working = true;
+    int choice{0};
+    
+    while(test._working) {
+        
+        words.clear();
+        std::cout << "Введите начало слова: ";
+        std::string prefix;
+        std::cin >> prefix;
+        
+        std::cout << "телеметрия: в векторе " << words.size() << " элементов\n";    //телеметрия
+        
+        //заполняем вектор доступными вариантами
+        test.find_words_for_prefixes(words, prefix);
+        
+        std::cout << "телеметрия: в векторе " << words.size() << " элементов\n";    //телеметрия
+        
+        //std::cout << words.back()<<'\n';
+        //words.pop_back();
+        
+        if ( words.empty() ) {
+            std::cout << "Not found\n";
+        }
+        
+        while ( !words.empty() ) {
+            
+            std::cout << "Do you want to choose word: " << words.back() << "?\n"
+            << "1 - YES, 2 - Next word, 0 - Abort the selection, 3 - Exit the program\n:";
+            std::cin >> choice;
+            
+            switch (choice) {
+                case 0:
+                    //Abort;
+                    words.clear();
+                    break;
+        
+                case 1:
+                    //YES;
+                    std::cout <<"You entered the word \"" << words.back() 
+                    << "\"\nWe continue to enter\n";
+                    words.clear();
+                    break;
+                
+                case 2:
+                    //Next word;
+                    words.pop_back();
+                    if (words.empty() ) {
+                        std::cout << "Not found\n";
+                    }
+                    break;
+                
+                case 3:
+                    //Exit the program;
+                    return;
+
+                default:
+                    std::cout <<"ERROR the enter, please repeat\n";
+                    break;
+            }
+        }
+    }
+}
